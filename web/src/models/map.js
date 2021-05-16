ade89d4a38a0ad02d2590c384246f51d4f10dfd5
Dearkano <vaynetian@gmail.com>
2021-05-16 02:14:19 -0400

    update host

--- web/src/models/map.js
+++ web/src/models/map.js
@@ -20,7 +20,7 @@ export default {
     *getData({ payload }, { put }) {
       const { mag, time } = payload;
       const res = yield fetch(
-        `http://localhost:7001/query?mag=${mag}&range=${time}`
+        `http://13.82.140.200:7001/query?mag=${mag}&range=${time}`
       );
       const data = yield res.json();
       yield put({ type: "save", payload: data });
@@ -28,7 +28,7 @@ export default {
     *predict({ payload }, { put }) {
       const { data } = payload;
       const res = yield fetch(
-        `http://localhost:7001/predict?mag=${data.mag || 0}&mmi=${
+        `http://13.82.140.200:7001/predict?mag=${data.mag || 0}&mmi=${
           data.mmi || 0
         }&sig=${data.sig || 0}&depth=${data.depth || 0}&n_foreshocks=${
           data.n_foreshocks || 0
